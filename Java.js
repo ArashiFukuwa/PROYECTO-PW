b82e82e233036e847d3b7a817c7ba3aff87aeeed
ArashiFukuwa <felipedroguettf@gmail.com>
2021-04-27 19:26:56 -0400

    Primer commit

---
+++ Java.js
@@ -0,0 +1,13 @@
+
+  var instance = M.Carousel.init({
+    fullWidth: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+  var instance = M.Tabs.init(el,options);
+
